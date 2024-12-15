078e88e9610a4350502e76ba8a279fc678d4ac3a
Oleksandr Ruppelt <alexanderruppelt@gmail.com>
2024-12-15 17:39:16 +0100

    fix of env variable

--- app/api/strength/route.ts
+++ app/api/strength/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/strength`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}for_against`)
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
